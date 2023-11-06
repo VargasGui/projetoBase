9408ebb11d274ea0c29bbb3ca3c5bce3d40b3d5d
VargasGui <guilherme.vargas1h3@gmail.com>
2023-11-06 12:31:14 -0300

    commit aula 06/11

--- HelperStockBeta/HelperStockBeta.Infra.Data/Repositories/CategoryRepository.cs
+++ HelperStockBeta/HelperStockBeta.Infra.Data/Repositories/CategoryRepository.cs
@@ -12,13 +12,14 @@ namespace HelperStockBeta.Infra.Data.Repositories
 {
     public class CategoryRepository : ICategoryRepository
     {
+        //Contexto de infra em conexão
         ApplicationDbContext _categoryContext;
-        public CategoryRepository(ApplicationDbContext context)
+
+        //Modelagem do contexto de conexão para Categoria
+        public CategoryRepository(ApplicationDbContext context) 
         {
             _categoryContext = context;
-
         }
-
         public async Task<Category> CreateAsync(Category category)
         {
             _categoryContext.Add(category);
@@ -33,7 +34,7 @@ namespace HelperStockBeta.Infra.Data.Repositories
 
         public async Task<IEnumerable<Category>> GetCategoriesAsync()
         {
-            return await _categoryContext.Categories.ToListAsync(); ;
+            return await _categoryContext.Categories.ToListAsync();
         }
 
         public async Task<Category> RemoveAsync(Category category)
